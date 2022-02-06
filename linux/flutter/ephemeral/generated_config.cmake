e2e385b120fc9972da61cc93c9784ccb7dfbfcc0
Danillo Ilggner <danilloilggner@gmail.com>
2022-02-06 13:53:20 -0300

    removed linux environment

---
+++ linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,15 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/opt/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/danilloilggner/StudioProjects/codelabs_first_app" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/opt/flutter"
+  "PROJECT_DIR=/home/danilloilggner/StudioProjects/codelabs_first_app"
+  "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/home/danilloilggner/StudioProjects/codelabs_first_app/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/home/danilloilggner/StudioProjects/codelabs_first_app/lib/main.dart"
+)
